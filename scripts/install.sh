0eeb33d8b49b5153002abef1ffbba1fcf5cbf973
Adil Mohiuddin <amohi9046@gmail.com>
2021-04-16 15:18:32 -0700

    package list

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,17 +1,8 @@
-# Setup
-
-## Cloning
-```bash
-git clone --recursive git@github.com:advilm/dotfiles.git
-```
-
-## Packages 
-```bash
+# Install paru
 sudo pacman -S --needed base-devel
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
-```
-```bash
+
+# Install rest of the packages
 paru -S xorg-xsetroot polybar feh picom flameshot clipmenu rofi dunst bspwm sxhkd pipewire pipewire-pulse pipewire pipewire-alsa pipewire-jack pipewire-media-session pulseeffects discord-development firefox-developer-edition alacritty ttf-liberation ttf-mac-fonts ttf-ms-fonts ttf-roboto ttf-symbola ttf-twemoji ttf-unifont ttf-windows ttf-bitstream-vera ttf-hack ttf-meslo-nerd-font-powerlevel10k dina-font siji zsh antibody xsel xdotool steam-native-runtime tealdeer rsync dust bat cpu-x 
-```
