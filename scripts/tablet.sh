b4be2f9c19e80d6da7bc9d093e53b3eeda489030
Adil Mohiuddin <amohi9046@gmail.com>
2021-11-03 17:39:31 -0700

    some key bindings for drawing tablet

---
+++ scripts/tablet.sh
@@ -0,0 +1,9 @@
+xsetwacom --set 9 Button 1 11
+xsetwacom --set 9 Button 2 12
+xsetwacom --set 9 Button 3 13
+xsetwacom --set 9 Button 8 14
+xsetwacom --set 9 Button 9 15
+xsetwacom --set 9 Button 10 16
+
+xsetwacom --set 8 Button 2 17
+xsetwacom --set 8 Button 3 18
