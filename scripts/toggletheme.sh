c6ccc4923f63d8a131f668c9bfe102953cb26256
Adil Mohiuddin <amohi9046@gmail.com>
2022-02-28 19:20:24 -0800

    major theme overhaul to add light theme toggle

---
+++ scripts/toggletheme.sh
@@ -0,0 +1,17 @@
+if [ "$(xfconf-query -c xsettings -p /Net/ThemeName)" = "Matcha-dark-azul" ]; then
+    xfconf-query -c xsettings -p /Net/ThemeName -s "Matcha-azul"
+    xfconf-query -c xfce4-notifyd -p /theme -s "Matcha-azul"
+    cp ~/.config/alacritty/alacritty-light.yml ~/.config/alacritty/alacritty.yml
+    cp ~/.config/polybar/config-light ~/.config/polybar/config
+    cp ~/.config/ulauncher/user-themes/advil/theme-light.css ~/.config/ulauncher/user-themes/advil/theme.css
+    pkill ulauncher; GDK_BACKEND=x11 ulauncher --hide-window --no-window-shadow &
+    hsetroot -solid '#ebedef'
+else
+    xfconf-query -c xsettings -p /Net/ThemeName -s "Matcha-dark-azul"
+    xfconf-query -c xfce4-notifyd -p /theme -s "Matcha-dark-azul"
+    cp ~/.config/alacritty/alacritty-dark.yml ~/.config/alacritty/alacritty.yml
+    cp ~/.config/polybar/config-dark ~/.config/polybar/config
+    cp ~/.config/ulauncher/user-themes/advil/theme-dark.css ~/.config/ulauncher/user-themes/advil/theme.css
+    pkill ulauncher; GDK_BACKEND=x11 ulauncher --hide-window --no-window-shadow &
+    hsetroot -solid '#020e1d'
+fi
