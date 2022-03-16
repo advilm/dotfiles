d3043b2c8af3cac7b3c1fdad39fac1974afc5d2f
Adil Mohiuddin <amohi9046@gmail.com>
2022-03-15 18:21:08 -0700

    fixed ulauncher light theme

--- scripts/toggletheme.sh
+++ scripts/toggletheme.sh
@@ -3,6 +3,7 @@ if [ "$(xfconf-query -c xsettings -p /Net/ThemeName)" = "Matcha-dark-azul" ]; th
     xfconf-query -c xfce4-notifyd -p /theme -s "Matcha-azul"
     cp ~/.config/alacritty/alacritty-light.yml ~/.config/alacritty/alacritty.yml
     cp ~/.config/polybar/config-light ~/.config/polybar/config
+    cp ~/.config/ulauncher/user-themes/advil/manifest-light.json ~/.config/ulauncher/user-themes/advil/manifest.json
     cp ~/.config/ulauncher/user-themes/advil/theme-light.css ~/.config/ulauncher/user-themes/advil/theme.css
     pkill ulauncher; GDK_BACKEND=x11 ulauncher --hide-window --no-window-shadow &
     hsetroot -solid '#ebedef'
@@ -11,6 +12,7 @@ else
     xfconf-query -c xfce4-notifyd -p /theme -s "Matcha-dark-azul"
     cp ~/.config/alacritty/alacritty-dark.yml ~/.config/alacritty/alacritty.yml
     cp ~/.config/polybar/config-dark ~/.config/polybar/config
+    cp ~/.config/ulauncher/user-themes/advil/manifest-dark.json ~/.config/ulauncher/user-themes/advil/manifest.json
     cp ~/.config/ulauncher/user-themes/advil/theme-dark.css ~/.config/ulauncher/user-themes/advil/theme.css
     pkill ulauncher; GDK_BACKEND=x11 ulauncher --hide-window --no-window-shadow &
     hsetroot -solid '#020e1d'
