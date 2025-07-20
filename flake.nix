982d7a7c24661203f8753361c9fd62dff3b0c2b5
Adil Mohiuddin <adil.mohiuddin07@gmail.com>
2025-07-20 16:18:58 -0700

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { nixpkgs, ... } @ inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./configuration.nix 
+      ];
+    };
+  };
+}
