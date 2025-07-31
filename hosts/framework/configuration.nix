18319d4b90677cd9d4d6d2a4e33d65e0609b20f9
Adil Mohiuddin <adil.mohiuddin07@gmail.com>
2025-07-31 14:47:03 -0700

    feat: enable cachix on laptop

--- hosts/framework/configuration.nix
+++ hosts/framework/configuration.nix
@@ -25,12 +25,22 @@
   ];
 
   hardware.framework.enableKmod = true;
-  hardware.framework.laptop13.audioEnhancement.enable = true;
+  # hardware.framework.laptop13.audioEnhancement.enable = true;
 
   # enable when doing firmware updates
   services.fwupd.enable = false;
 
   console.keyMap = "dvorak";
+
+  # use package cache on laptop
+  nix.settings = {
+    substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
 
   # limit to laptop keyboard
   services.keyd.keyboards.default.ids = [ "0001:0001:70533846" ];
