3bf08aedc2570596a4f319240028f82e34a1e1fc
Adil Mohiuddin <adil.mohiuddin07@gmail.com>
2025-08-05 00:04:16 -0700

    fix: bind xf86audioprev properly

--- modules/home-manager/desktops/hyprland/default.nix
+++ modules/home-manager/desktops/hyprland/default.nix
@@ -81,7 +81,7 @@
           ]
         ) ++ [
         ", XF86AudioNext, exec, playerctl next"
-        ", XF86AudioPrev, exec, playerctl prev"
+        ", XF86AudioPrev, exec, playerctl previous"
         ", XF86AudioPlay, exec, playerctl play-pause"
         ", XF86AudioPause, exec, playerctl play-pause"
       ];
