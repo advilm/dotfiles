99218fd079c995a5662e14459e354c1809e88c9f
Adil Mohiuddin <adil.mohiuddin07@gmail.com>
2025-08-16 20:36:12 -0700

    feat: add tv profile to kanshi

--- modules/home-manager/services/kanshi/default.nix
+++ modules/home-manager/services/kanshi/default.nix
@@ -6,7 +6,6 @@ _: {
         output = {
           criteria = "BOE 0x095F Unknown";
           mode = "2256x1504@59.99900";
-          position = "0,0";
           scale = 1.333333;
         };
       }
@@ -14,14 +13,25 @@ _: {
         output = {
           criteria = "Dell Inc. DELL G2724D 82K16Y3";
           mode = "2560x1440@165.080002";
-          position = "0,0";
           scale = 1.0;
+        };
+      }
+      {
+        output = {
+          criteria = "Samsung Electric Company SAMSUNG 0x01000E00";
+          mode = "3840x2160@30.000000";
+          scale = 2.0;
         };
       }
       {
         profile = {
           name = "desktop";
-          outputs = [ { criteria = "Dell Inc. DELL G2724D 82K16Y3"; } ];
+          outputs = [
+            {
+              criteria = "Dell Inc. DELL G2724D 82K16Y3";
+              status = "enable";
+            }
+          ];
         };
       }
       {
@@ -47,7 +57,21 @@ _: {
               criteria = "Dell Inc. DELL G2724D 82K16Y3";
               status = "enable";
               # mode = "2560x1440@59.95100";
-              # position = "2560,0";
+            }
+          ];
+        };
+      }
+      {
+        profile = {
+          name = "laptop-tv";
+          outputs = [
+            {
+              criteria = "BOE 0x095F Unknown";
+              status = "disable";
+            }
+            {
+              criteria = "Samsung Electric Company SAMSUNG 0x01000E00";
+              status = "enable";
             }
           ];
         };
