fbb900eb70afc4b061648d03139fc27d455be2a0
Adil Mohiuddin <adil.mohiuddin07@gmail.com>
2025-08-21 07:17:06 -0700

    feat: enable xdg-terminal-exec and set alacritty as default

--- modules/home-manager/misc/xdg/default.nix
+++ modules/home-manager/misc/xdg/default.nix
@@ -1,14 +1,25 @@
 _: {
   xdg = {
     enable = true;
+
     mimeApps = {
       enable = true;
       defaultApplications = {
         "x-scheme-handler/http" = [ "firefox.desktop" ];
         "x-scheme-handler/https" = [ "firefox.desktop" ];
         "text/html" = [ "firefox.desktop" ];
+        "text/plain" = [ "nvim.desktop" ];
       };
     };
+
+    # set alacritty as the preferred terminal
+    terminal-exec = {
+      enable = true;
+      settings = {
+        default = [ "Alacritty.desktop" ];
+      };
+    };
+
     # portal = {
     #   enable = true;
     #   xdgOpenUsePortal = true;
