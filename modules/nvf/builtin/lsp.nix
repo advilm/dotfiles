60f2f52729fe94a781bbf7b88c4cdd62f0cf6161
Adil Mohiuddin <adil.mohiuddin07@gmail.com>
2025-08-22 23:43:36 -0700

    feat(nvf): set blink keymap, no more tabbing

--- modules/nvf/builtin/lsp.nix
+++ modules/nvf/builtin/lsp.nix
@@ -1,4 +1,5 @@
 _:
+
 {
   vim.lsp.enable = true;
   vim.lsp.otter-nvim.enable = true;
@@ -6,8 +7,46 @@ _:
   vim.autocomplete.blink-cmp = {
     enable = true;
     setupOpts = {
-      fuzzy.implementation = "rust";
-      cmdline.keymap.preset = "default";
+      keymap = {
+        preset = "none";
+        "<C-space>" = [ "show" "show_documentation" "hide_documentation" ];
+    
+        "<C-e>" = [ "hide" ];
+        "<C-y>" = [ "select_and_accept" ];
+
+        "<C-p>" = [ "select_prev" ];
+        "<C-n>" = [ "select_next" ];
+
+        "<C-b>" = [ "scroll_documentation_up" ];
+        "<C-f>" = [ "scroll_documentation_down" ];
+
+        "<C-k>" = [ "show_signature" "hide_signature" ];
+      };
+      cmdline.keymap = {
+        "<C-space>" = [ "show" "show_documentation" "hide_documentation" ];
+        "<Tab>" = [ "show_and_insert" "select_next" ];
+        "<S-Tab>" = [ "show_and_insert" "select_prev" ];
+    
+        "<C-e>" = [ "hide" ];
+        "<C-y>" = [ "select_and_accept" ];
+
+        "<C-p>" = [ "select_prev" ];
+        "<C-n>" = [ "select_next" ];
+
+        "<C-b>" = [ "scroll_documentation_up" ];
+        "<C-f>" = [ "scroll_documentation_down" ];
+
+        "<C-k>" = [ "show_signature" "hide_signature" ];
+      };
+    };
+    mappings = {
+      close = null;
+      complete = null;
+      confirm = null;
+      next = null;
+      previous = null;
+      scrollDocsDown = null;
+      scrollDocsUp = null;
     };
   };
 }
