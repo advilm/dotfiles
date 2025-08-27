1898023ac94a854867f7a7f83783ecf237bd62a0
Adil Mohiuddin <adil.mohiuddin07@gmail.com>
2025-08-27 13:12:38 -0700

    feat: add and set matcha azul qt theme

--- modules/home-manager/misc/gtk/default.nix
+++ modules/home-manager/misc/gtk/default.nix
@@ -18,6 +18,28 @@
     gtk3.extraConfig.gtk-application-prefer-dark-theme = 1;
   };
 
+  qt = {
+    enable = true;
+    platformTheme.name = "qt5ct";
+    style.name = "kvantum";
+  };
+
+  xdg.configFile = {
+    "Kvantum/Matcha-azul-dark".source = "${
+      (pkgs.fetchFromGitHub {
+        owner = "R0CKH0PP3R";
+        repo = "kvantum";
+        rev = "23a6178562634a596d2d39e7d69beeb54ee4c650";
+        sha256 = "sha256-wKIglQ/v5anxNM9je+Li+BT3BQJFO46HMDHQSCv6IzA=";
+      })
+    }/Matcha-azul-dark";
+
+    "Kvantum/kvantum.kvconfig".text = ''
+      [General]
+      theme=Matcha-azul-dark
+    '';
+  };
+
   dconf.settings = {
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
