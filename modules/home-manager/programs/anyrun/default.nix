91e661f6090f0fe960abfae6dcfabcc6edd87d1d
Adil Mohiuddin <adil.mohiuddin07@gmail.com>
2025-09-03 00:41:25 -0700

    feat: configure anyrun theme and add calc plugin

--- modules/home-manager/programs/anyrun/default.nix
+++ modules/home-manager/programs/anyrun/default.nix
@@ -20,15 +20,62 @@
 
       plugins = [
         inputs.anyrun.packages.${pkgs.system}.applications
+        inputs.anyrun.packages.${pkgs.system}.rink
         inputs.anyrun.packages.${pkgs.system}.nix-run
       ];
     };
 
-    # Inline comments are supported for language injection into
-    # multi-line strings with Treesitter! (Depends on your editor)
     extraCss = ''
-      #window {
+      window {
         background: transparent;
+      }
+
+      box.main {
+        padding: 5px;
+        margin: 10px;
+        border: 2px solid @theme_selected_bg_color;
+        background-color: @theme_bg_color;
+        box-shadow: 0 0 5px black;
+      }
+
+      text {
+        font-size: 20px;
+        min-height: 30px;
+        padding: 5px;
+        border-radius: 5px;
+      }
+
+      .matches {
+        background-color: rgba(0, 0, 0, 0);
+      }
+
+      list.plugin {
+        background-color: rgba(0, 0, 0, 0);
+      }
+
+      list.plugin row {
+        margin-top: 2px;
+        margin-left: 12px;
+        background: transparent;
+      }
+
+      list.plugin row:selected {
+        border-left: 2px solid @theme_selected_bg_color;
+        margin-left: 4px;
+        padding-left: 6px;
+        background: @theme_bg_color;
+      }
+
+      list.plugin row:first-child {
+        margin-top: 0px;
+      }
+
+      label.match.title {
+        font-size: 20px;
+      }
+
+      label.match.description {
+        font-size: 16px;
       }
     '';
 
