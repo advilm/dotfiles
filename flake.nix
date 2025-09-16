33600d01f3c5f9045cb87c6c0c8d1076f8741269
Adil Mohiuddin <adil.mohiuddin07@gmail.com>
2025-09-15 21:00:49 -0700

    feat: update anyrun input to use fork

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
     nix-index-database.url = "github:nix-community/nix-index-database";
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
 
-    anyrun.url = "github:anyrun-org/anyrun";
+    anyrun.url = "github:advilm/anyrun";
     anyrun.inputs.nixpkgs.follows = "nixpkgs";
 
     nvf.url = "github:notashelf/nvf";
